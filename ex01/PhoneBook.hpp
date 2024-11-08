bd3b3213300d1d88fe86039f62f1101159469153
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2024-11-08 13:59:32 +0800

    build: now all Makefiles uses `g++` instead of `clang++` chore: fixed weird file

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -1,19 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjun-yu <tjun-yu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 10:45:41 by tjun-yu           #+#    #+#             */
+/*   Updated: 2024/10/24 13:03:05 by tjun-yu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 # pragma once
-# ifndef __PHONEBOOK_HPP__
-	# define __PHONEBOOK_HPP__
+# ifndef PHONEBOOK_HPP
+#  define PHONEBOOK_HPP
 
-	# include "Contact.hpp"
-	# include <string>
-	# include <iostream>
+# include <limits>
+# include <cstdlib>
+# include "Contact.hpp"
 
-	class PhoneBook
-	{
-		private:
-			Contact	arr[8];
+class PhoneBook
+{
+	private:
+		Contact _contacts[8];
 
-		public:
-			void	add_contact(const Contact& contact);
-			Contact	get_contact(int index) const;
-	};
+	public:
+		void	add_contact(Contact &contact);
+		void	print_contact(int index);
+		void	print_contact_list(void);
+		Contact	*get_contact(int index);
+};
+
+std::string	prompt(const std::string &msg);
+int			add(PhoneBook &phonebook);
+int			search(PhoneBook &phonebook);
 
 # endif
